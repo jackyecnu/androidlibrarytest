71aa7accfd6da86f6e96088821d892414a56b237
jackyzhong <jackyecnu@126.com>
2021-06-12 11:09:10 +0800

    init

---
+++ app/src/main/java/com/lingman/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lingman.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lingman.utils.DemonTestHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.tvTest);
+        textView.setText(String.valueOf(DemonTestHelper.Plus1(1)));
+    }
+}
---
+++ utils/src/main/java/com/lingman/utils/DemonTestHelper.java
@@ -0,0 +1,9 @@
+package com.lingman.utils;
+
+public class DemonTestHelper {
+
+    public  static  int Plus1(int i) {
+        return  i + 2;
+    }
+
+}
